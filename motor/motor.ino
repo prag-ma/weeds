76f26fcb4ba945a54140451885b001dd3abd14d7
Erica Jewell <ejewe001@gold.ac.uk>
2016-09-11 15:22:19 +0100

    Remove line count
    Line count was originally a debugging variable.

--- motor/motor.ino
+++ motor/motor.ino
@@ -33,10 +33,12 @@ void setup() {
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
+//  pinMode(7, OUTPUT);
   Serial.begin(9600);
+  
+  // set digital pin to 0 to send a ground signal to the adafruit soundboard, to start it playing the loop
+//  pinMode(7, LOW);
 }
-
-
 
 void loop(){
   count++;
@@ -47,7 +49,7 @@ void loop(){
     setOutput(i);
     delayMicroseconds(motorSpeed);
     // save <count> to EEPROM
-    Serial.println(count);
+    //Serial.println(count);
   }
   if (count == countsperrev){
     Serial.println("revolution");
